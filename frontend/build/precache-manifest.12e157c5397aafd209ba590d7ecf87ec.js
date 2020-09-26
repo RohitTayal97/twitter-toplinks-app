79b1223fda41b9b95c84a1cae8736de98d8da7ff
Rohit Tayal <tayal.rohit14@gmail.com>
2020-09-27 02:23:05 +0530

    creating build

---
+++ frontend/build/precache-manifest.12e157c5397aafd209ba590d7ecf87ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5cd359595c2653cec3ea",
+    "url": "/static/css/main.474152f3.chunk.css"
+  },
+  {
+    "revision": "5cd359595c2653cec3ea",
+    "url": "/static/js/main.5cd35959.chunk.js"
+  },
+  {
+    "revision": "8c091bd9b31707d33063",
+    "url": "/static/js/1.8c091bd9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3a7d36fe1fe381f718a5b315b5b2dbf6",
+    "url": "/index.html"
+  }
+];
